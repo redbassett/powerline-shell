825bb816ba58d82bbe4adb631b0bb477090f1b3d
Harry Grillo <harry@redbassett.com>
2017-08-04 02:11:50 -0400

    Customize git dirty/clean branch colors

--- themes/default.py
+++ themes/default.py
@@ -32,9 +32,9 @@ class DefaultColor:
     SSH_BG = 166 # medium orange
     SSH_FG = 254
 
-    REPO_CLEAN_BG = 148  # a light green color
+    REPO_CLEAN_BG = 40
     REPO_CLEAN_FG = 0  # black
-    REPO_DIRTY_BG = 161  # pink/red
+    REPO_DIRTY_BG = 196
     REPO_DIRTY_FG = 15  # white
 
     JOBS_FG = 39
